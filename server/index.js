cdd4ae4914afbc7016282739a4c31e1122cce7ec
Olga Matosova <matosovaolga@gmail.com>
2024-04-19 13:37:21 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ app.use(cookieParser());
 
 app.use("/api/v1", routes);
 
-// const port = process.env.PORT || 6000;
+const port = process.env.PORT || 6000;
 
 const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
   console.log("Mongodb connected");
-  server.listen(6000, () => {
-    console.log(`Server is listening on port ${6000}`);
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
   });
 }).catch((err) => {
   console.log({ err });
