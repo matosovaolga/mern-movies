b2c3dc3699a9fbc9517acaf75ae9fec97e97694d
Olga Matosova <matosovaolga@gmail.com>
2024-04-19 14:00:04 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -19,14 +19,28 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(3000, () => {
-    console.log(`Server is listening on port ${3000}`);
-  });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//   console.log("Mongodb connected");
+//   server.listen(3000, () => {
+//     console.log(`Server is listening on port ${3000}`);
+//   });
+// }).catch((err) => {
+//   console.log({ err });
+//   process.exit(1);
+// });
+
+const start = async () => {
+  try {
+    mongoose.connect(process.env.MONGODB_URL);
+    app.listen(3000, () => {
+      console.log(`Server start on port ${3000}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
+module.exports = app;
 
 //test
