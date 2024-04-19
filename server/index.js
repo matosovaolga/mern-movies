d770874b847e8bd249a767293476410f0b4aa10c
Olga Matosova <matosovaolga@gmail.com>
2024-04-19 14:24:10 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,46 +1,8 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import http from "http";
-import mongoose from "mongoose";
-// import "dotenv/config";
-// import routes from "./src/routes/index.js";
-
+const express = require("express");
 const app = express();
 
-// app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// app.use("/api/v1", routes);
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// const port = normalizePort(process.env.PORT || 3000);
-
-// const server = http.createServer(app);
-
-// mongoose.connect(process.env.MONGODB_URL).then(() => {
-//   console.log("Mongodb connected");
-//   server.listen(3000, () => {
-//     console.log(`Server is listening on port ${3000}`);
-//   });
-// }).catch((err) => {
-//   console.log({ err });
-//   process.exit(1);
-// });
-
-const start = async () => {
-  try {
-    mongoose.connect(process.env.MONGODB_URL);
-    app.listen(3000, () => {
-      console.log(`Server start on port ${3000}`);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-start();
 module.exports = app;
-
-//test
