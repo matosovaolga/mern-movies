2bb9823f48dcaf14a07dfe744acf09c8bec2adfd
Olga Matosova <matosovaolga@gmail.com>
2024-04-19 16:30:01 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 import cookieParser from "cookie-parser";
+import http from "http";
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
