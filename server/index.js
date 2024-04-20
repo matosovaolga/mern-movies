ba834d5245b0d54889b3535078232303a9e8e0ac
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 15:47:04 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel kik"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
