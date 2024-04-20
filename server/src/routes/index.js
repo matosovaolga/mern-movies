772face9648814dada5358608b364bdd4e839371
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 17:39:43 +0300

    fix

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-// const userRoute = require("./user.route.js");
+const userRoute = require("./user.route.js");
 // const mediaRoute = require("./media.route.js");
 // const personRoute = require("./person.route.js");
 // const reviewRoute = require("./review.route.js");
 
 const router = express.Router();
 
-// router.use("/user", userRoute);
+router.use("/user", userRoute);
 // router.use("/person", personRoute);
 // router.use("/reviews", reviewRoute);
 // router.use("/:mediaType", mediaRoute);
