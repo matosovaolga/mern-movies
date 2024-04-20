894eee6702c5151f20a4895a1181463172012044
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 17:35:11 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const cookieParser = require("cookie-parser");
 const cors =  require("cors");
 const http = require("http");
 const dotenv =  require("dotenv/config");
-const routes = require("./src/routes/index.js");
+// const routes = require("./src/routes/index.js");
 
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
