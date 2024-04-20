3631654bfe3abdcb8340a18542d202066ddca6e5
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 16:55:15 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors =  require("cors");
 const http = require("http");
+const dotenv =  require("dotenv/config");
 
 
 const app = express();
