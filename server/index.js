a8f2123dd48c911bea9ddb647a70d134d9568d89
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 16:51:49 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const http = require("http");
 
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
