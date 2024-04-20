92b58bc1554c22e0bec91d72a328371fe6214a9f
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 16:15:34 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel kik"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app;
