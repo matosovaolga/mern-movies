2ae8a65adf4e151a1f960ca9c11c3802930edf66
Olga Matosova <matosovaolga@gmail.com>
2024-04-20 17:36:14 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -8,10 +8,10 @@ const dotenv =  require("dotenv/config");
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
