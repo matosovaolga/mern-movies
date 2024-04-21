e578325fc4b5bc90906efecad3abca8c28f9cd5d
Olga Matosova <matosovaolga@gmail.com>
2024-04-21 12:36:06 +0300

    fix

--- server/src/handlers/request.handler.js
+++ server/src/handlers/request.handler.js
@@ -1,11 +1,14 @@
-const { validationResult } = require("express-validator");
+const expressValidator = require("express-validator");
+
+const validationResult = expressValidator.validationResult;
 
 const validate = (req, res, next) => {
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()) return res.status(400).json({
-    message: errors.array()[0].msg
-  });
+  if (!errors.isEmpty())
+    return res.status(400).json({
+      message: errors.array()[0].msg,
+    });
 
   next();
 };
